3a5201c0a8b3e9173e19ed32ca4200ce94e44146
TomasDroppa <droppa_tomas@oauh.cz>
2022-11-09 17:53:58 +0100

    první commit

---
+++ application/views/layout/menu.php
@@ -0,0 +1,58 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="<?php echo base_url(); ?>">Hlavní stránka</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarColor01">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="prvni-stranka">Link 1
+                                    
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="druha-stranka">Link 2</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="treti-stranka">Link 3</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="ctvrta-stranka">Link 4</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Dropdown 1</a>
+                                    <a class="dropdown-item" href="#">Dropdown 2</a>
+                                    <a class="dropdown-item" href="#">Dropdown 3</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Dropdown 4</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ml-auto">
+                             <li class="nav-item">
+                                <a class="nav-link" href="registrace"><i class="fas fa-user-plus"></i> Registrace
+                                    
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                
+                                <a class="nav-link" href="prihlaseni"><i class="fas fa-sign-in-alt"></i> Přihlášení</a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </nav>   
+
+
+
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
